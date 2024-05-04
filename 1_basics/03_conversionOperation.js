b3868fdc8bcf93c219ce6b367f281dbc5a07be3e
shriyanshurawat <04shriyanshu@gmail.com>
2024-05-04 12:43:09 +0530

    Create 03_conversionOperation.js
    conversion of the datatype

---
+++ 1_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let score = "Shriyanshu"
+
+// console.log(typeof(score));
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+let isLoggedIn = true;
+
+let booleanIsloggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsloggedIn);
+
+// 1=> true; 0 => flase;
+
+//"" => false
+//"Shriyanshu" => true
+
+
