19df827726e09cad5b64118eff81de205b42fdd7
shriyanshurawat <04shriyanshu@gmail.com>
2024-05-04 16:40:17 +0530

    Update 03_conversionOperation.js

--- 1_basics/03_conversionOperation.js
+++ 1_basics/03_conversionOperation.js
@@ -9,15 +9,51 @@
 // console.log(valueInNumber);
 
 
-let isLoggedIn = true;
+// let isLoggedIn = true;
 
-let booleanIsloggedIn = Boolean(isLoggedIn);
+// let booleanIsloggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
 
 // 1=> true; 0 => flase;
 
 //"" => false
 //"Shriyanshu" => true
 
+//***********************OPERATIONS************************//
 
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);//these signs are use is cryptograpy many times 
+// console.log(2%3);// this sign too used in cryptography 
+
+// let str1 ="hello"
+// let str2 = " Shriyanshu"
+
+// console.log(str1+str2)
+// console.log(1+1);
+// console.log(1+2+3+4);
+// console.log("1"+2+3+4); // This is not a good practice to write a code
+// console.log(2+3+4+"1"+2+4+"5"); // This is not a good practice to write a code 
+
+// console.log(+true); // not a good practice
+// console.log(+""); // this too
+// console.log()
+
+/* so here suddenly a think stricks in my brain that what is the difference betweent the console.log and the doucment.write so the main diff.
+is that document.write changes the structure of the page like it can modify the html sourcecode and the conosle.log have nothing to do with it*/
+
+//Clean and the Readable code is the good practice
+
+// let num1 , num2 , num3
+// num1 = num2 = num3 = 2+2 
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
