69eb9cc113ebe49971acb89196c2c1819d361ad8
shriyanshurawat <04shriyanshu@gmail.com>
2024-05-05 16:21:44 +0530

    just basic comparision
    just start learning them

---
+++ 04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2>1);
+// console.log(2>=1);
+
+// console.log("2">1);    // THESE TYPES OF CONVERSION ARE NOT SUITABLE TO USE THEM
+// console.log(null == 0);
+// console.log(null>=0);
+
+// console.log(undefined == 0);
+//in every aspect of life of the undefined will be false bec.. it doesnt exist hahahahahhaha
+
+// === ? it is strict check this see the datatypes of the varibale too to compare them
+console.log("2" ===2);
