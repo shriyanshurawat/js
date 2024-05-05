5fdb4520e6602e2c776a10c503fe0044c72328af
shriyanshurawat <04shriyanshu@gmail.com>
2024-05-05 16:39:52 +0530

    another one

---
+++ DATATYPES-summry.js
@@ -0,0 +1,33 @@
+// primtive data types
+// 7 types :String , number , boolean , null , undefined, Symbol, BigInt 
+
+// const score = 100
+// const scoreValue = 100.3
+
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id ===anotherId);
+
+const BigNumber = 2134123412451235123n //automatically  use bigInt if dont use n at the last of the number
+
+
+
+
+// // Reference (Non primitive)
+
+// // Arrays , objects , Functions 
+
+// const heros = ["shaktiman" , "naagraja" , "jaadu"]
+
+// let myObj = {
+//     name: "Shriyanshu",
+//     age: 21
+// }
+
+const myFunction  = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof BigNumber);
