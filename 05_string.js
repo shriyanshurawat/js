12d4f9241da61581704a2dcb21536689892372dd
shriyanshurawat <04shriyanshu@gmail.com>
2024-05-07 17:28:22 +0530

    stringggg
    2:11

---
+++ 05_string.js
@@ -0,0 +1,7 @@
+const name = "Shriyanshu"
+const repoCount = "50"
+
+console.log(name + repoCount+ "value");
+ 
+// another and goog way of adding multiple strings together
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
--- DATATYPES-summry.js
+++ DATATYPES-summry.js
@@ -26,16 +26,37 @@ const BigNumber = 2134123412451235123n //automatically  use bigInt if dont use n
 //     age: 21
 // }
 
-const myFunction  = function(){
-    console.log("Hello world");
-}
+// const myFunction  = function(){
+//     console.log("Hello world");
+// }
 
-console.log(typeof BigNumber);
+// console.log(typeof BigNumber);
 
 /*****************************************************************************************/
 
 //Stack memory use in (primitve) - it means give a copy of what we change
+// numbers and boolean 
+ 
+let myname = "Shriyanshhu"
+let anothername = "Shriyanshu"
+anothername = "Rawat"
+
+console.log(myname)
+console.log(anothername)
+
+let userone ={
+    email: "user@google.com" , 
+    upi: "user@upl"
+}
+
+let usertwo = userone;
+usertwo.email = "shriyanshu@gmal.com"
+
+console.log(usertwo.email);
+console.log(userone.email);
+
+
 
 //heap (non -  primitive) - it means when we change this datatype it entierly change the value 
 
-let myschoolName = 
+// let myschoolName = 
