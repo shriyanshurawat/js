0d818ea6cd8077ec7059c4921b8192d3db7c8c86
shriyanshurawat <04shriyanshu@gmail.com>
2024-05-09 10:09:39 +0530

    Update 05_string.js
    2:21

--- 05_string.js
+++ 05_string.js
@@ -1,7 +1,26 @@
-const name = "Shriyanshu"
-const repoCount = "50"
+// const name = "Shriyanshu"
+// const repoCount = "50"
 
-console.log(name + repoCount+ "value");
+// console.log(name + repoCount+ "value");
  
-// another and goog way of adding multiple strings together
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+// // only the good way of concatinate multiple strings together
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+// another way to define a string 
+const gameName = new String ('Shriyanshu')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+console.log(gameName.charAt(0));
+console.log(gameName.indexOf('y'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+console.log("_______________________");
+const anotherString = gameName.slice(-1);
+console.log(anotherString)
