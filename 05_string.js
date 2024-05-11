9f4adcb4cb947fa5fcfca570e5af5b97342d6bd0
shriyanshurawat <04shriyanshu@gmail.com>
2024-05-11 10:25:45 +0530

    studied more

--- 05_string.js
+++ 05_string.js
@@ -8,19 +8,27 @@
 
 
 // another way to define a string 
-const gameName = new String ('Shriyanshu')
+// const gameName = new String ('Shriyanshu')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase);
-console.log(gameName.charAt(0));
-console.log(gameName.indexOf('y'));
+// console.log(gameName.charAt(0));
+// console.log(gameName.indexOf('y'));
 
-const newString = gameName.substring(0,4);
-console.log(newString);
+// const newString = gameName.substring(0,4);
+// console.log(newString);
 
-console.log("_______________________");
-const anotherString = gameName.slice(-1);
-console.log(anotherString)
+
+// const anotherString = gameName.slice(-1);
+// console.log(anotherString)
+
+const newStringone = "       shriynashu   "
+console.log(newStringone);
+
+console.log(newStringone.trim()); /// simply remove the empty space and perfrom on whitespace and line 
+
+const url = "https://Shriyanshu.com/shriyanshu%20rawat"
+console.log(url.replace('%20','___')) // it will replace the %20 from ___
