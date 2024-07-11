6c29baa38ae00e6ba818f46def3478578eb57998
shriyanshurawat <04shriyanshu@gmail.com>
2024-07-11 20:15:14 +0530

    practice
    lalalalalalalaa

---
+++ 06_nums_and_maths.js
@@ -0,0 +1,13 @@
+const score = 400 
+const balance = new Number(100); // we defined it that is a number so it should be number and we have now few function for it to apply on it as compare to stiring
+
+console.log(balance);
+
+// so now here the value is the same but we defined its data type to be in string by which we are now able to apply many inbuild function in it
+console.log(balance.toString().length);
+console.log(balance.toString());
+console.log(balance.toFixed(2)); // output 400.00
+console.log(typeof(balance));
+
+const otherNumber = 23.8966
+vondolr.h
